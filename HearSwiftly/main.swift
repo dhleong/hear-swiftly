597fc3bf7661f200ccbf74883af6f82d060c9243
dhleong <falcone88@gmail.com>
2015-05-11 16:05:10 -0400

    Initial Commit

---
+++ HearSwiftly/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HearSwiftly
+//
+//  Created by Daniel Leong on 5/11/15.
+//  Copyright (c) 2015 Dan Leong Apps. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
